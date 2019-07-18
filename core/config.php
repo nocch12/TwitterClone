3322ba1fd5e5649e0d9634d8ef788e81f6915fae
Ohno <k.ohno12.gt@gmail.com>
2019-07-18 12:35:03 +0900

    debug

---
+++ core/config.php
@@ -0,0 +1,13 @@
+<?php
+
+ini_set('display_errors', 1);
+
+define('DSN', 'mysql:host=localhost;dbname=mybbs;charset=utf8');
+define('DB_USER', 'ohnoman');
+define('DB_PASS', 'ew5KnK64EAqs');
+
+
+define('MAX_FILE_SIZE', 1 * 1024 * 1024);
+define('THUMBNAIL_WIDTH', 400);
+define('POST_IMAGES_DIR', str_replace('/core', '', __DIR__) . '/posted_images');
+define('USER_IMAGES_DIR', str_replace('/core', '', __DIR__) . '/user_images');
