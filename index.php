7f0562c57113c64cc26f4962772a3416d7bbc1bb
Ohno <k.ohno12.gt@gmail.com>
2019-07-19 01:26:09 +0900

    dbg

--- index.php
+++ index.php
@@ -4,10 +4,8 @@ session_start();
 require_once(__DIR__ . '/core/config.php');
 require_once(__DIR__ . '/functions.php');
 
-var_dump($_SESSION);
-exit;
 
-if(empty($_SESSION['id'])) {
+if(!isset($_SESSION['id'])) {
     header('Location: login.php');
 }
 
