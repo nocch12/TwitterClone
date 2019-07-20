ca0df25fde19e51aab7a24a95031e550059720f9
Ohno <k.ohno12.gt@gmail.com>
2019-07-20 22:47:08 +0900

    aws

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -47,6 +47,24 @@ class ComposerAutoloaderInit0d3be6726408f7d14dbffb9e51d2b438
 
         $loader->register(true);
 
+        if ($useStaticLoader) {
+            $includeFiles = Composer\Autoload\ComposerStaticInit0d3be6726408f7d14dbffb9e51d2b438::$files;
+        } else {
+            $includeFiles = require __DIR__ . '/autoload_files.php';
+        }
+        foreach ($includeFiles as $fileIdentifier => $file) {
+            composerRequire0d3be6726408f7d14dbffb9e51d2b438($fileIdentifier, $file);
+        }
+
         return $loader;
+    }
+}
+
+function composerRequire0d3be6726408f7d14dbffb9e51d2b438($fileIdentifier, $file)
+{
+    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+        require $file;
+
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
     }
 }
